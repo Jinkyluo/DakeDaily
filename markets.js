3992ff45bb4c6a36688bcc9f3a8b3f49ae5538ed
Jinkyluo <153840456+Jinkyluo@users.noreply.github.com>
2026-09-18 16:09:42 +0800

    Publish A-share close brief for 2026-09-18

--- markets.js
+++ markets.js
@@ -1,4 +1,4 @@
 window.dailyMarkets = {
   us: {path: 'us-morning-2026-09-18.html', date: '2026-09-18'},
-  cn: {path: 'a-share-2026-09-17.html', date: '2026-09-17'}
+  cn: {path: 'a-share-2026-09-18.html', date: '2026-09-18'}
 };
